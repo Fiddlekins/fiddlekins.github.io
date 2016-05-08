5c6affd35d6da11b7195bd38813a6e93fc0a2ad6
Fiddlekins <fiddlekins@gmail.com>
2016-05-08 23:34:15 +0100

    Added help command, made commands case insensitive, extended cd functionality

--- js/inputHandler.js
+++ js/inputHandler.js
@@ -11,17 +11,21 @@ terminal.inputHandler.updateCurrentDirectoryString = function(){
 terminal.inputHandler.process = function(string){
 	terminal.content.pushLine(terminal.inputController.currentInputString);
 	var args, target;
-	if (/^cd( |$)/.test(string)) {
+	if (/^cd( |$)/i.test(string)) {
 		args = string.slice(3);
-		target = terminal.inputHandler.currentDirectory.changeDirectory(args.split('/').reverse());
-		if (typeof target === 'string') {
-			terminal.content.pushLine(target);
+		if (/^ *$/.test(args)) {
+			terminal.content.pushLine(terminal.inputHandler.currentDirectoryString.slice(0, terminal.inputHandler.currentDirectoryString.length - 1));
 		} else {
-			terminal.inputHandler.currentDirectory = target;
-			terminal.inputHandler.updateCurrentDirectoryString();
-			terminal.inputController.updateCurrentInputString();
+			target = terminal.inputHandler.currentDirectory.changeDirectory(args.split('/').reverse());
+			if (typeof target === 'string') {
+				terminal.content.pushLine(target);
+			} else {
+				terminal.inputHandler.currentDirectory = target;
+				terminal.inputHandler.updateCurrentDirectoryString();
+				terminal.inputController.updateCurrentInputString();
+			}
 		}
-	} else if (/^dir( |$)/.test(string)) {
+	} else if (/^dir( |$)/i.test(string)) {
 		args = string.slice(4);
 		if (args.length && args !== ' ') {
 			target = terminal.inputHandler.currentDirectory.changeDirectory(args.split('/').reverse());
@@ -30,6 +34,8 @@ terminal.inputHandler.process = function(string){
 		}
 		var contents = target.listContents();
 		terminal.content.concatLines(contents);
+	} else if (/^help( |$)/i.test(string)) {
+		terminal.content.concatLines(terminal.inputHandler.help);
 	} else {
 		var targetDirectory = string.split('/');
 		var targetFile = targetDirectory.pop();
@@ -41,3 +47,9 @@ terminal.inputHandler.process = function(string){
 		}
 	}
 };
+
+terminal.inputHandler.help = [
+	'CD     Displays the name of or changes the current directory.',
+	'DIR    Displays a list of files and subdirectories in a directory.',
+	'HELP   Provides Help information for commands.'
+];
